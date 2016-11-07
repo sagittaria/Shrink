f5d2acb90b17264f13c778d4c89c7b9da1f3762b
kasoya <shuyang_x@hotmail.com>
2016-11-07 16:21:20 +0800

    daily

--- Primer/stage01-basic/daily.cpp
+++ Primer/stage01-basic/daily.cpp
@@ -1,27 +1,56 @@
-#include <iostream>
-#include "Sales_item.h"
+#include <stdio.h>
 int main()
 {
-	Sales_item total;
-	if (std::cin >> total){
-		Sales_item trans;
-		while (std::cin >> trans){
-			if (total.isbn == trans.isbn)
-				total += trans;
-			else{
-				std::cout << total << std::endl;
-				total = trans;
-			}
+	int a = 0, b = 0;
+	int mod = 0, qu = 0;
+	scanf_s("%d/%d", &a, &b);
+	printf("0.");
+	int i;
+	for (i = 0; i < 200; i++)
+	{
+		qu = a * 10 / b;
+		mod = a * 10 % b;
+		if (mod != 0){
+			printf("%d", qu);
+			a = mod;
 		}
-		std::cout << total << std::endl;
-	}
-	else {
-		std::cerr << "No data?!" << std::endl;
-		return -1;
+		else{
+			break;
+		}
 	}
 	return 0;
+	
 }
-	/*
+
+/*
+int i,sum=0;
+for (i = 0; i < 3; i++){
+sum += i;
+printf("%d", sum);
+}
+int b = 9;
+b = 4;
+b = 10;
+return 0;
+-------------------
+	Sales_item total;
+	if (std::cin >> total){
+	Sales_item trans;
+	while (std::cin >> trans){
+	if (total.isbn == trans.isbn)
+	total += trans;
+	else{
+	std::cout << total << std::endl;
+	total = trans;
+	}
+	}
+	std::cout << total << std::endl;
+	}
+	else {
+	std::cerr << "No data?!" << std::endl;
+	return -1;
+	}
+	-------------
 	Sales_item item1, item2;
 	std::cin >> item1 >> item2;
 	std::cout << item1 + item2 << std::endl;
@@ -47,4 +76,4 @@ int main()
 	std::cout << currVal << " occurs " << cnt << " times " << std::endl;
 	}
 	return 0;
-	*/
+*/
